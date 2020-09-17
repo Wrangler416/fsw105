f54a3524c656c85f679cc34383d5a213af14cf7e
Kara Temple <kara.temple@bryanuniversity.edu>
2020-09-17 11:44:34 -0400

    strings

---
+++ week4/stringarray_methods/index.js
@@ -0,0 +1,41 @@
+//1. 
+var newString = "Hello"
+var upper = newString.toUpperCase()
+var lower = newString.toLowerCase()
+console.log(upper + lower)
+
+//2.  
+
+var number = "136944"
+var numberHalf = number.slice(0, 3)
+var numberHalfRounded = Math.floor(numberHalf)
+
+console.log(numberHalf)
+console.log(numberHalfRounded)
+
+//3. 
+
+var nextString = "Web Development"
+var halfLength = Math.ceil(nextString.length / 2); 
+var splicedString = nextString.slice(0, halfLength)
+
+console.log(splicedString)
+
+//4. 
+
+var lastString = "Full Stack Web Development"
+var firstHalf = lastString.slice(0, 13)
+var lastHalf = lastString.slice(13, 26 )
+var capFirst = firstHalf.toUpperCase()
+var capLast = lastHalf.toLowerCase()
+
+console.log(capFirst + capLast) 
+
+
+
+
+
+
+
+
+
