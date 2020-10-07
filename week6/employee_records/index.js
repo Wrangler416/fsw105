c846d4038ce6fceffab9b9e9faf1257386f823cf
Kara Temple <kara.temple@bryanuniversity.edu>
2020-10-07 19:54:55 -0400

    redo

--- week6/employee_records/index.js
+++ week6/employee_records/index.js
@@ -1,18 +1,19 @@
 const employees = [];
 
-function Employed(name, jobtitle, salary, status) {
+function Employed(name, jobtitle, salary, status="Full-Time") {
             this.name = name;
             this.jobtitle = jobtitle;
             this.salary = salary;
             this.status = status;
             this.printEmployeeForm = function() {return this.name + " " + this.jobtitle + " " + this.salary + " " + this.status}   
 }
-var employee1 = new Employed("Bob", "Manager", 7000, "Full-Time");
-var employee2 = new Employed("Mary", "Sales Manager", 5000, "Full-Time");
-var employee3 = new Employed("Steve", "Assistant Manager", 4000, "Full-Time");
+var employee1 = new Employed("Bob", "Manager", 7000);
+var employee2 = new Employed("Mary", "Sales Manager", 5000);
+var employee3 = new Employed("Steve", "Assistant Manager", 4000);
 
 employee1.status = "Part-Time"
 
-console.log(employee1.printEmployeeForm());
-console.log(employee2.printEmployeeForm());
-console.log(employee3.printEmployeeForm()); 
+let toArray = employee1.printEmployeeForm() + " " + employee2.printEmployeeForm() + " " + employee3.printEmployeeForm();
+
+employees.push(toArray);
+console.log(employees)
