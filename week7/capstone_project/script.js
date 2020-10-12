520d0cabc85b38c0e868a66cfe3265273e399745
Kara Temple <kara.temple@bryanuniversity.edu>
2020-10-11 21:31:35 -0400

    cap

--- week7/capstone_project/script.js
+++ week7/capstone_project/script.js
@@ -5,12 +5,12 @@ console.log(welcome);
 
 const playerName = readlineSync.question("Please enter your name");
 
-const welcome2 = readlineSync.question("Welcome" + playerName + ", we are headed into the depths of the Amazon! Let's see if you have what it takes to survive, Press Enter to continue"); 
+const welcome2 = readlineSync.question("Welcome" + " " + playerName + "!, we are headed into the depths of the Amazon.  Let's see if you have what it takes to survive, Press enter to continue.."); 
 console.log(welcome2);
 
 const enemies = ["Jaguar", "Anaconda", "Caiman", "Posion Dart Frog", "Piranha"];
 const healing = ["Wasai", "Tawari", "Lapacho", "Sodo"];
-const menu = ["Walk", "Rescue Me", "Inventory"]
+const menu = ["Hike", "Rescue", "Health"]
 let award = []; 
 
 let playerHealth = 50;
@@ -18,55 +18,58 @@ let lottery = healing[Math.floor(Math.random() * healing.length)];
 
 
 function game() {
-    const playerKillPower = Math.floor(Math.random() * (10 + 6 * 2)); 
-    const enemyKillPower = Math.floor(Math.random() * (7 - 2 * 6)); 
+    const playerKillPower = Math.floor(Math.random() * (4 + 3 * 8)); 
 
-    let enemy = enemies[Math.floor(Math.random() * enemies.length)];
+    const enemy = enemies[Math.floor(Math.random() * enemies.length)];
+
     let enemyHealth = 50; 
+
+    const enemyKillPower = Math.floor(Math.random() * (5 - 2 * 7)); 
 
     const option = readlineSync.keyInSelect(menu, "Choose one of the following options");
 
-    if (menu[option] == "Rescue Me") {
+    if (menu[option] === "Rescue") {
         return playerHealth = 0; 
-    } else if (menu[option] == "Inventory") {
-        console.log(playerName + "" + "Health = " + playerHealth + ":\n Healers Collected = " + healing)
-    } else if (menu[option] == "Walk"){
+    } else if (menu[option] === "Health") {
+        console.log(playerName + " " + "'s Health: " + playerHealth + "\n Healers Collected: " + healing);
+    } else if (menu[option] === "Hike"){
         let chance = Math.random();
         if(chance <= .2) {
-            console.log("Walking . . .");
+            console.log("Ok now we are hiking the jungle . . .");
         } else if (chance >= .2) {
-            console.log("Uh oh, a/an" + enemy + "has appeared!");
+            console.log("Uh oh, a/an" + " " + enemy + " " + "has appeared!");
 
             while(enemyHealth > 0 && playerHealth > 0) {
 
                 const choice = readlineSync.question("What are you going to do?! You can try to escape or you can fight. Press e to escape and f to fight!");
+
                     switch(choice) {
 
                         case 'e':
                             const escape = Math.random();
-                        if(run <.5) {
-                            console.log("Oh no, you were not fast enough!" + enemy + "has attacked you with" + enemyKillPower);
-                        } else {
+                        if(escape <.5) {
+                            console.log("Oh no, you were not fast enough!" + " " + enemy + " " + "has attacked you with" + " " + enemyKillPower);
+                        }else {
                             console.log("Awesome, you got away! This time...");
                             break;
                         }
 
                         case 'f':
-                            enemyHealth - playerKillPower;
-                            console.log("Nice!, you attacked" + enemy + "with" + playerKillPower + "of power!");
+                            enemyHealth -= playerKillPower;
+                            console.log("Nice!, you attacked" + " " + enemy + " " + "with" + " " + playerKillPower + " " + "of power!");
 
-                            playerHealth - enemyKillPower; 
-                            console.log("Your enemy," + " " + enemy + " " + "attacked with" + enemyKillPower); 
+                            playerHealth -= enemyKillPower; 
+                            console.log("Your enemy," + " " + enemy + " " + "attacked with" + " " + enemyKillPower); 
 
                             if(enemyHealth <= 0) {
-                                console.log("You killed" + " " + enemy + " " + "You are rewarded with" + lottery); 
+                                console.log("You killed" + " " + enemy + " " + "You are rewarded with" + " " + lottery); 
                                 let prize = Math.random();
-                                if(price <= .5) {
+                                if(prize <= .5) {
                                     award.push(lottery);
                                 }
  
                             } else if(playerHealth <= 0) {
-                                console.log("The jungle has defeated you, game over")
+                                console.log("The jungle has defeated you, game over");
                             }
 
 
@@ -84,7 +87,7 @@ while(playerHealth > 0) {
     playerHealth = 50; 
     };
 
-    playerRestor();
+    playerRestore();
     game(); 
 };
 
